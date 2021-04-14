e6c345bb068dcefd60402d563720057dcc15a710
AdrianSilvadoNascimento <75761636+AdrianSilvadoNascimento@users.noreply.github.com>
2021-04-13 22:34:21 -0300

    Calculadora de Gorjetas
    A calculadora é um projeto para ajudar restaurantes a não perderem na hora de cobrar e o(s) cliente(s) for/forem dividir a conta. Calcula também o valor da porcentagem em gorjeta para os serviços prestados.

---
+++ calculador de gorjetas/scripts.js
@@ -0,0 +1,29 @@
+function calculateTip(event){
+    event.preventDefault();
+    let bill = document.getElementById('valor').value;
+    let service = document.getElementById('service').value;
+    let numOfPeople = document.getElementById('people').value; 
+    if(bill == '' | service == 0){
+        alert("Por favor, preencha todos os campos!")
+        return;
+    } else if(bill < 1){
+        alert("Por favor, preencher com valor válido!")
+        return;
+    }
+    if(numOfPeople == "" | numOfPeople < 1){
+        numOfPeople = 1;
+        document.getElementById('each').style.display = "none"
+    }   else if(numOfPeople <= 1){
+        document.getElementById('each').style.display = "none"
+    }
+     else {
+        document.getElementById('each').style.display = "block"
+    }
+    let total = (bill * service) / numOfPeople;
+    total = total.toFixed(2);
+    document.getElementById('tip').innerHTML = total;
+    document.getElementById('totalTip').style.display = "block";
+}
+document.getElementById('totalTip').style.display = "none";
+document.getElementById('each').style.display = "none";
+document.getElementById('tipsForm').addEventListener('submit', calculateTip);
